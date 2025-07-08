ce5be7d30134c67058a3a5e83624a123fcabd98f
DzaboffCodes <142997851+DzaboffCodes@users.noreply.github.com>
2025-07-08 09:57:01 -0500

    Added functionality to the logo to go back to the home page after searching a topic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import { FaReddit } from 'react-icons/fa';
 import { Link } from 'react-router-dom'; // <-- Add this import
 import SearchBar from './SearchBar';
+import { useDispatch } from 'react-redux';
+import { fetchPosts } from '../features/posts/postSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleLogoClick = () => {
+    dispatch(fetchPosts());
+  };
+
   return (
     <div className="header">
       <div className="logo-title">
-        <Link to="/"> {/* Make the icon clickable */}
+        <Link to="/" onClick={handleLogoClick}> 
           <FaReddit size={32} color="#FF4500" style={{ cursor: 'pointer' }} />
         </Link>
         <h1>Reddit Viewer</h1>
